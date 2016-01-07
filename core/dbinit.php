c928b1f70197727a1e818b4e81105ba68d5e94b5
Andre Herberth Skjervik Thoresen <andre.thoresen@gmail.com>
2016-01-07 10:38:19 +0100

    Create dbinit.php

---
+++ core/dbinit.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+Main Database config file.
+Created by Andre Herberth on 07.01.2016
+Updated last by Andre Herberth on 07.01.2016
+*/
+
+//Edit Below
+
+dbinit_dbname = ""; //Insert your database name
+dbinit_dbprefix = ""; //Insert database prefix if any. 
+dbinit_dbuser = ""; //Insert Database username
+dbinit_dbpwd = ""; //Insert Database Password. 
+//Edit Above
+
+//DO NOT EDIT ANYTHING BELOW!
